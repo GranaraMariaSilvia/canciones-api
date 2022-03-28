2cfd6334deb2ccd38d458da6d57302b1f4f8bf3e
Maria Silvia <granarasilvia10@gmail.com>
2022-03-28 19:20:55 -0300

    se termino con la seccion storage

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,20 +1,44 @@
 
-
-const {storageModels} = require('../models')
+const fs = require('fs');
+const { matchedData } = require('express-validator');
+const {storageModels} = require('../models');
+const { handleHttpError } = require('../utils/handleError');
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 const getItems = async (req,res)=>{
-    const data = await storageModels.find({})
+
+    try {
+        const data = await storageModels.find({})
+
+        res.send({data})
+        
+    } catch (error) {
+        handleHttpError(res, 'Error_Get_Items')
+    }
+};
+
+
+const getItem = async(req,res)=>{
+try {
+
+    const {id} = matchedData(req)
+    const data = await storageModels.findById(id)
 
     res.send({data})
-
+} catch (error) {
+    handleHttpError(res, 'Error_Detail_Item')
 }
-const getItem = (req,res)=>{
+
+};
 
 
-}
+
 const createItem = async (req,res)=>{
+
+try {
+    
     const {body,file} = req
     console.log(file)
 const fileData = {
@@ -25,11 +49,39 @@ const fileData = {
 }
     const data = await storageModels.create(fileData)
     res.send({data})
+    
+} catch (error) {
+    handleHttpError(res, 'Error_Crear_Item')
 }
 
-
-const updateItems = (req,res)=>{}
-const deleteItems = (req,res)=>{}
+};
 
 
-module.exports ={ getItems,getItem,createItem,updateItems,deleteItems}
+
+
+const deleteItem = async (req,res)=>{
+    try {
+
+       
+        const {id} = matchedData(req)
+        const dataFile = await storageModels.findById(id)
+
+        await storageModels.delete({_id:id}) //con esto lo elimino de la bd pero queda guardado mantengo la persistencia
+    
+          const {filename} = dataFile //saco el nombre del archivo
+       const filePath = `${MEDIA_PATH}/${filename}` //aqui tengo la ruta mas nombre del archivo
+      // fs.unlinkSync(filePath) con esto se elimina el registro fisico y no lo recupero 
+    const data ={
+        filePath,
+        deleted:1
+
+    }
+
+        res.send({data})
+    } catch (error) {
+        handleHttpError(res, 'Error_Delete_Item')
+    }
+};
+
+
+module.exports ={ getItems,getItem,createItem,deleteItem}
--- routes/storage.js
+++ routes/storage.js
@@ -1,12 +1,29 @@
 
 const express = require('express');
+const {validatorGetItem} =require('../validator/storage')
 
 const upload = require('../utils/handleStorage')
 const router = express.Router();
-const {createItem} = require('../controllers/storage')
+const {createItem,getItem,getItems,deleteItem,updateItem} = require('../controllers/storage');
 
 
+
+
+//listar items
+router.get('/', getItems)
+
+//detalle de un item
+router.get('/:id', validatorGetItem, getItem);
+
+
+//crear un item
 router.post('/',upload.single("myFile"),createItem)
+
+
+//router.put('/:id', updateItem)
+
+//eliminar item
+router.delete('/:id', validatorGetItem, deleteItem);
 
 
 module.exports = router
