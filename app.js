0cd72a1ce817277fe57cba360180959e2f54b6fa
Maria Silvia <granarasilvia10@gmail.com>
2022-04-04 17:08:51 -0300

    se implemento el aviso de errores en slack

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ require("dotenv").config()
 const express = require ("express");
 const cors = require("cors");
 const dbConnect = require('./config/mongo');
+const loggerStream = require('./utils/handleLogger')
 const app = express();
+const morganBody = require('morgan-body');
 
 
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("storage"));
+
+
+
+
+
+
+
+
+morganBody(app, {
+    noColors:true,
+    stream: loggerStream, 
+    skip: function (req,res){
+       return res.statusCode < 400
+    }
+})
 
 
 const port = process.env.PORT || 3000;
---
+++ utils/handleLogger.js
@@ -0,0 +1,19 @@
+
+const {IncomingWebhook} = require('@slack/webhook');
+
+
+const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOk)
+
+
+const loggerStream = {
+
+    write: message => {
+        webHook.send({
+            text: message
+        });
+        console.log('capturando el LOG', message)
+      
+    },
+  };
+
+  module.exports = loggerStream
